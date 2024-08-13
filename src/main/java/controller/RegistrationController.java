e32a1f057a6abe5ea5e4a47470e9f986bd6dcd9f
Maxim Nikolus <nikolusmaxim409@gmail.com>
2024-08-13 21:27:15 +0200

    Update

---
+++ src/main/java/controller/RegistrationController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import service.UserService;
+
+import java.util.Scanner;
+
+public class RegistrationController {
+
+    private Scanner scanner;
+    private UserService userService;
+
+
+
+}

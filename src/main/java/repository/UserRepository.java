10f77e166b9b212deafa473803dd415d949470c3
wadim-81 <v1281@mail.ru>
2024-08-16 10:12:03 +0200

    new2

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -3,10 +3,11 @@ package repository;
 import entity.User;
 
 public interface UserRepository {
-
-    boolean isUsername(String login);
-
+    boolean isUserExists(String login);
     void saveUser(User user);
-
     User getUserByUsername(String login);
+
+
+
 }
+
--- src/main/java/repository/UserRepositoryImpl.java
+++ src/main/java/repository/UserRepositoryImpl.java
@@ -1,32 +1,16 @@
 package repository;
 
+import com.google.common.hash.Hashing;
 import entity.User;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
 public class UserRepositoryImpl implements UserRepository {
+
     private final Map<String, User> usersDatabase = new HashMap<>();
 
 
-    @Override
-    public boolean isUsername(String login) {
-        return usersDatabase.containsKey(login);
-        /**добавляет объект user в мапу usersDatabase заменяя старое значение на новое*/
 
     }
-
-    @Override
-    public void saveUser(User user) {
-         usersDatabase.put(user.getUserName(), user);
-    /**строка добавляет объект User в usersDatabase, используя имя пользователя в качестве ключа.*/
-    }
-
-    @Override
-    public User getUserByUsername(String login) {
-        return usersDatabase.get(login);
-        /**Эта строка кода возвращает объект User, связанный с указанным именем пользователя, из базы данных usersDatabase.*/
-    }
-}
-
-
